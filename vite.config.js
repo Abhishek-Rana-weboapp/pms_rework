c1a129d18f004e0230136d28d1185ed6b2529d3c
Abhishek Rana <arana0944@gmail.com>
2026-07-30 12:22:09 +0530

    vercel config fixed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
@@ -24,12 +20,11 @@ export default defineConfig(({ mode }) => {
     port: 5173,
     proxy: {
       "/api": {
-        target: env.VITE_API_URL,
+        target: "http://110.225.254.51:4040",
         changeOrigin: true,
         secure: false,
       },
     },
   }
-}
 }
 )
